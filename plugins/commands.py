a75b619851698d86bbe7d944fbc15538e3a892fa
piro-dev010 <109207523+piro-dev010@users.noreply.github.com>
2022-08-13 02:08:29 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,6 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=
         protect_content=True if pre == 'filep' else False,
         )
                     
